beb0ec7b03d7bb3a8a18d6683a1aa97a7d934f71
bbeetlesam <samsismines@gmail.com>
2025-07-22 02:32:34 +0700

    refactor: add stepping sound and change movement from lerp to linear
    linear looks more good with the eyes tbh, rather than lerp that makes my eyes a bit dizzy

--- src/core/player.lua
+++ src/core/player.lua
@@ -10,8 +10,10 @@ function Player:load(x, y, playable)
     self.playable = playable
 
     self.isMoving = false
-    self.speed = 16 -- higher is faster
-    self.stepSize = 30
+    self.speed = 150 -- higher is faster
+    self.stepSize = 32
+
+    self.stepSound = love.audio.newSource("assets/sfx/" .. "walk-step.wav", "static")
 end
 
 function Player:update(dt)
@@ -35,13 +37,23 @@ function Player:update(dt)
 
     -- while moving, move towards the target with interpolation (lerp)
     if self.isMoving then
-        self.x = self.x + (self.targetX - self.x) * self.speed * dt
-        self.y = self.y + (self.targetY - self.y) * self.speed * dt
+        local dx = self.targetX - self.x
+        local dy = self.targetY - self.y
+        local dist = math.sqrt(dx * dx + dy * dy)
 
-        -- check if it is close enough to stop.
-        if math.abs(self.targetX - self.x) < 0.5 and math.abs(self.targetY - self.y) < 0.5 then
+        if dist < self.speed * dt then
             self.x, self.y = self.targetX, self.targetY -- snap to the final position
             self.isMoving = false
+
+            -- add stepping sound
+            self.stepSound:play()
+            self.stepSound:setVolume(0.3)
+        else
+            -- move towards the target at a constant speed.
+            local dirX = dx / dist
+            local dirY = dy / dist
+            self.x = self.x + dirX * self.speed * dt
+            self.y = self.y + dirY * self.speed * dt
         end
     end
 end
