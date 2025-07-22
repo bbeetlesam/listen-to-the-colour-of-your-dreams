5fa45394c275dda2186a8c633f06108c31718648
bbeetlesam <samsismines@gmail.com>
2025-07-23 01:29:40 +0700

    refactor: const in const.lua

--- src/helpers/const.lua
+++ src/helpers/const.lua
@@ -5,10 +5,11 @@ local const = {
     WIDTH = 1280, -- main canvas's width
     HEIGHT = 720, -- main canvas's height
     LINE_WIDTH = 4,
+    TILE_SIZE = 32,
 
     -- colors
-    BROKEN_WHITE = {utils.rgb(248, 247, 243)},
-    BLACK = {utils.rgb(0, 0, 0)},
+    BROKEN_WHITE = utils.rgb(248, 247, 243, 255),
+    BLACK = utils.rgb(0, 0, 0, 255),
 }
 
 return const
