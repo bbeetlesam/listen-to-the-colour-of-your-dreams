14409c54cf69ad807f2e259164966b8d1b4e93ce
bbeetlesam <samsismines@gmail.com>
2025-07-23 01:29:13 +0700

    add some stuffs in utils.lua

--- src/helpers/utils.lua
+++ src/helpers/utils.lua
@@ -1,43 +1,114 @@
 local utils = {
+    draw = {
+        stairs = function (x, y, depth, direction, color, size, linewidth)
+            local const = require("src.helpers.const")
+            depth = depth or 1
+            size = size or 32
+            linewidth = linewidth or const.LINE_WIDTH
+            direction = direction or "right"
+            color = color or require("src.states").lineColor
+
+            local dir = direction == "right" and 1 or -1
+
+            local points = {x, y}
+            for i = 1, depth - 0 do
+                -- add the corner point at the bottom of the vertical riser
+                table.insert(points, x + (i - 1) * size * dir)
+                table.insert(points, y + i * size)
+
+                -- add the corner point at the end of the horizontal tread
+                table.insert(points, x + i * size * dir)
+                table.insert(points, y + i * size)
+            end
+
+            love.graphics.push("all")
+            love.graphics.setLineWidth(linewidth)
+            love.graphics.setColor(color)
+            love.graphics.line(points)
+            love.graphics.pop()
+        end,
+
+        mailbox = function (x, y, size)
+            local const = require("src.helpers.const")
+            local linewidth = const.LINE_WIDTH
+            local color = require("src.states").lineColor
+            local a, b = 5, 25
+
+            love.graphics.push("all")
+            love.graphics.setLineWidth(linewidth)
+            love.graphics.setColor(color)
+            love.graphics.line({x-a,y, x-a,y-const.TILE_SIZE*1.5}) -- pole
+            love.graphics.line({x+a,y, x+a,y-const.TILE_SIZE*1.5}) -- pole
+            love.graphics.line({x-b+26,y-const.TILE_SIZE*2, x-b+26,y-const.TILE_SIZE*1.5}) -- vert
+            love.graphics.line({x-b+12,y-const.TILE_SIZE*2-12, x+b-12,y-const.TILE_SIZE*2-12}) -- horz
+            love.graphics.line({x-b,y-const.TILE_SIZE*2, x-b,y-const.TILE_SIZE*1.5, x+b,y-const.TILE_SIZE*1.5, x+b,y-const.TILE_SIZE*2}) -- mailbox
+            love.graphics.arc("line", "open", x - 13, y - const.TILE_SIZE*2, 12, math.rad(-90), math.rad(-180))
+            love.graphics.arc("line", "open", x + 13, y - const.TILE_SIZE*2, 12, 0, -math.pi)
+            love.graphics.pop()
+        end,
+    },
+
     rgb = function (r, g, b, a)
-        return love.math.colorFromBytes(r, g, b, a)
+        return {love.math.colorFromBytes(r, g, b, a)}
     end,
 
     lines = function (points, color)
         love.graphics.push("all")
         love.graphics.setLineWidth(require("src.helpers.const").LINE_WIDTH)
-        love.graphics.setColor(color or {0, 0, 0})
+        love.graphics.setColor(color or require("src.states").lineColor)
         love.graphics.line(points)
         love.graphics.pop()
     end,
 
-    drawStairs = function (x, y, depth, direction, color, size, linewidth)
-        local const = require("src.helpers.const")
-        depth = depth or 1
-        size = size or 32
-        linewidth = linewidth or const.LINE_WIDTH
-        direction = direction or "right"
-        color = color or const.BLACK
-
-        local dir = direction == "right" and 1 or -1
-
-        local points = {x, y}
-        for i = 1, depth - 0 do
-            -- add the corner point at the bottom of the vertical riser
-            table.insert(points, x + (i - 1) * size * dir)
-            table.insert(points, y + i * size)
-
-            -- add the corner point at the end of the horizontal tread
-            table.insert(points, x + i * size * dir)
-            table.insert(points, y + i * size)
+    lerpColor = function(color1, color2, t)
+        t = math.max(0, math.min(1, t)) -- clamp t between 0 and 1
+        local newColor = {}
+        for i = 1, 4 do
+            newColor[i] = color1[i] + (color2[i] - color1[i]) * t
         end
+        return newColor
+    end,
 
-        love.graphics.push("all")
-        love.graphics.setLineWidth(linewidth)
-        love.graphics.setColor(color)
-        love.graphics.line(points)
-        love.graphics.pop()
+    coordStr = function (x, y)
+        return x .. "," .. y
     end,
 }
+
+-- for tilemap debugging
+function utils.drawGrid(gridSize, color)
+    local const = require("src.helpers.const")
+    local screenW, screenH = const.WIDTH, const.HEIGHT
+    local centerX, centerY = screenW / 2 - 16, screenH / 2 - 16
+
+    gridSize = gridSize or 32
+    color = color or {0.8, 0.8, 0.8, 0.5}
+
+    love.graphics.push()
+
+    love.graphics.translate(centerX, centerY)
+
+    love.graphics.setColor(color)
+    love.graphics.setLineWidth(1)
+
+    local maxX = math.ceil(screenW / 2 / gridSize) * gridSize
+    local maxY = math.ceil(screenH / 2 / gridSize) * gridSize
+
+    -- vertical lines
+    for x = -maxX, maxX, gridSize do
+        love.graphics.line(x, -maxY, x, maxY)
+    end
+
+    -- horizontal lines
+    for y = -maxY, maxY, gridSize do
+        love.graphics.line(-maxX, y, maxX, y)
+    end
+
+    love.graphics.setColor(0, 0, 0, 0.8)
+    love.graphics.line(-4, 0, 4, 0)
+    love.graphics.line(0, -4, 0, 4)
+
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.pop()
+end
 
 return utils
