76de776cacd463278cf5e0db4b21b2d6cbd83f02
bbeetlesam <samsismines@gmail.com>
2025-07-25 03:27:34 +0700

    add rockHit sfx

--- src/sounds.lua
+++ src/sounds.lua
@@ -3,6 +3,7 @@ local sounds = {}
 function sounds.load()
     sounds.doorOpen = love.audio.newSource("assets/sfx/door-open.ogg", "static")
     sounds.doorClose = love.audio.newSource("assets/sfx/door-closed.ogg", "static")
+    sounds.rockHit = love.audio.newSource("assets/sfx/hit-rock.ogg", "static")
 end
 
 return sounds
