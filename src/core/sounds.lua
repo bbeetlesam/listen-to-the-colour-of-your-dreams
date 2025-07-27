60a9ac5e87a674f5599f8dc190f32a28554530c2
bbeetlesam <samsismines@gmail.com>
2025-07-27 22:03:18 +0700

    add boomRock and crowd sounds

--- src/core/sounds.lua
+++ src/core/sounds.lua
@@ -4,6 +4,8 @@ function sounds.load()
     sounds.doorOpen = love.audio.newSource("assets/sfx/door-open.ogg", "static")
     sounds.doorClose = love.audio.newSource("assets/sfx/door-closed.ogg", "static")
     sounds.rockHit = love.audio.newSource("assets/sfx/hit-rock.ogg", "static")
+    sounds.rockBoom = love.audio.newSource("assets/sfx/boom-rock.ogg", "static")
+    sounds.crowd = love.audio.newSource("assets/sfx/crowd-screams.ogg", "stream")
 end
 
 return sounds
