502fdae91f80f87b47121e3520e1b5a8c7349108
bbeetlesam <samsismines@gmail.com>
2025-07-27 22:07:16 +0700

    set conf.lua before submitting

--- conf.lua
+++ conf.lua
@@ -5,14 +5,16 @@ function love.conf(t)
     t.window.icon = nil
     t.window.width = 1920/1.2
     t.window.height = 1200/1.2
-    t.window.fullscreen = false
+    t.window.minwidth = 1280/2
+    t.window.minheight = 720/2
+    t.window.fullscreen = true
     t.window.resizable = true
 
     t.window.msaa = 0
     t.window.vsync = 1
 
     -- for debugging
-    t.console = true -- set to false on final build
-    t.window.display = 2 -- set to 1 on final build
+    t.console = false -- set to false on final build
+    t.window.display = 1 -- set to 1 on final build
     t.identity = nil
 end
